2132e2ba8a549976fe55dc102925785628b0dfaf
PRO0FESSOR <siddharth96549@gmail.com>
2025-03-26 13:28:46 +0530

    comments

---
+++ Python/learning/file_06.py
@@ -0,0 +1,17 @@
+
+# comments in python 
+
+    # it is a  
+    # comment  
+    # extending to multiple lines
+
+        # Code to show how we use docstrings in Python  
+      
+    def add(x, y):  
+        """This function adds the values of x and y"""  
+        return x + y  
+       
+    # Displaying the docstring of the add function  
+    print( add.__doc__ )
+
+
