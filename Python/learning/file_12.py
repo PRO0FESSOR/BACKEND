027928f45d48de17a3c49dcdab8dcd16fd64d136
PRO0FESSOR <siddharth96549@gmail.com>
2025-03-26 13:31:40 +0530

    pass

---
+++ Python/learning/file_12.py
@@ -0,0 +1,11 @@
+
+# pass 
+
+    # Python program to show how to use a pass statement in a for loop  
+    '''''pass acts as a placeholder. We can fill this place later on'''  
+    sequence = {"Python", "Pass", "Statement", "Placeholder"}  
+    for value in sequence:  
+        if value == "Pass":  
+            pass # leaving an empty if block using the pass keyword  
+        else:  
+            print("Not reached pass keyword: ", value)
